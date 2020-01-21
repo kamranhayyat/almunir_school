30869fe9e0c6a69fd1a05100f76dffd3fb4a31f3
kamranhayyat <kamranhayyat93@gmail.com>
2020-01-21 13:50:25 -0800

    changes

--- resources/views/children/show_children.blade.php
+++ resources/views/children/show_children.blade.php
@@ -8,13 +8,14 @@
            List Students
         </div>
         <div class="card-body">
+            <div class="table-responsive">
             <table class="table table-bordered table-striped">
                 <thead>
                     <th>ID</th>
                     <th>Full name</th>
-                    <th>Father name</th>
+                    {{-- <th>Father name</th>
                     <th>Date of birth</th>
-                    <th>Class / Section</th>
+                    <th>Class / Section</th> --}}
                     <th>Operations</th>
                 </thead>
                 <tbody>
@@ -26,16 +27,19 @@
                     @endif
                     @foreach($childrens as $children)
                     <tr>
-                        <td><i style="color:{{ $children['status'] == 1 ? 'green' : 'red'}}" class="fas fa-lightbulb mr-2"></i>{{$children['reg_no']}}</td>
+                        <td><i style="color:{{ $children['status'] == 1 ? 'green' : 'red'}}" class="fas fa-lightbulb mr-2"></i>
+                            <img src="{{asset('images/kid.jpg')}}" alt="" style="max-width:40px; border-radius: 5px;">
+                            {{$children['reg_no']}}</td>
                         <td>{{$children['student_name']}}</td>
-                        <td>{{$children['father_name']}}</td>
+                        {{-- <td>{{$children['father_name']}}</td>
                         <td>{{$children['dob']}}</td>
-                        <td>{{$children['class'] . ' / ' . $children['section']}}</td>
-                        <td><a href="{{ route('edit-student', ['id' => base64_encode($children['id'])]) }}" class="btn btn-info btn-sm text-white">View Student</a></td>
+                        <td>{{$children['class'] . ' / ' . $children['section']}}</td> --}}
+                        <td><a href="{{ route('edit-student', ['id' => base64_encode($children['id'])]) }}" class="btn btn-info btn-sm text-white">View</a></td>
                     </tr>
                     @endforeach
                 </tbody>
             </table>
+            </div>
             {{-- pagination --}}
             {{-- {{$childrens->links()}} --}}
         </div>
